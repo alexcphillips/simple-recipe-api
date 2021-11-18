19b4fb18196e54f2640f4e219f700ba2843fb7ec
Alexander Phillips <alexphillipsdev@gmail.com>
2021-11-17 19:58:48 -0600

    adds get recipe details route

--- server/index.js
+++ server/index.js
@@ -4,15 +4,35 @@ app.use(express.json());
 let { recipes } = require("./data.json");
 
 app.get("/recipes", (req, res) => {
-  let result = {
-    recipeNames: []
-  };
+  let recipeNames = [];
 
+  // Push name values into recipeNames
   for (let i = 0; i < recipes.length; i++) {
     result.recipeNames.push(recipes[i].name);
   }
 
-  res.send(result).status(200);
+  // Send names
+  res.send({ recipeNames }).status(200);
+});
+
+app.get("/recipes/details/:string", (req, res) => {
+  // Check if desired recipe exists
+  let desiredRecipe = recipes.find((recipe) => {
+    return recipe.name === req.params.string;
+  });
+
+  // If false, send empty object
+  if (!desiredRecipe) {
+    res.send({}).status(200);
+  } else {
+    // Send ingredients and instructions in response with status 200
+    res
+      .send({
+        ingredients: desiredRecipe.ingredients,
+        instructions: desiredRecipe.instructions
+      })
+      .status(200);
+  }
 });
 
 app.listen(3000, () => {
