1c81f4ec58a584ae20e13fd6705a93264c33283d
Alexander Phillips <alexphillipsdev@gmail.com>
2021-11-17 21:50:52 -0600

    adds testing directory and seperates the listener from the app

--- server/index.js
+++ server/index.js
@@ -1,12 +1,5 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
+const app = require("./app");
 
-const routes = require("./routes");
-
-app.use("/", routes);
 app.listen(3000, () => {
   console.log("Connected to server SUCCESSFULLY");
 });
-
-module.exports = app;
