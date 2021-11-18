8029ed344c3cce0060136d1aca22bb848d6d10aa
Alexander Phillips <alexphillipsdev@gmail.com>
2021-11-17 19:53:20 -0600

    adds get recipes route

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 let { recipes } = require("./data.json");
+
+app.get("/recipes", (req, res) => {
+  let result = {
+    recipeNames: []
+  };
+
+  for (let i = 0; i < recipes.length; i++) {
+    result.recipeNames.push(recipes[i].name);
+  }
+
+  res.send(result).status(200);
+});
+
+app.listen(3000, () => {
+  console.log("Connected to server SUCCESSFULLY");
+});
