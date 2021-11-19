2076204c60b0d4fa8a412bd0be34728f68e57eff
Alexander Phillips <alexphillipsdev@gmail.com>
2021-11-19 01:34:33 -0600

    moves PUT callback function to routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,22 +5,6 @@ const router = express.Router();
 router.get("/recipes", recipes.findAll);
 router.get("/recipes/details/:string", recipes.findDetails);
 router.post("/recipes", recipes.addOne);
-
-router.put("/recipe", (req, res) => {
-  let isExisting = false;
-  // Iterate through our recipes data, checking for desired object
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].name === req.body.name) {
-      isExisting = true;
-      // Set desired object to request body
-      data[i] = req.body;
-      res.status(204).send();
-      break;
-    }
-  }
-  if (!isExisting) {
-    res.status(404).send({ error: "Recipe does not exist" });
-  }
-});
+router.put("/recipe", recipes.updateOne);
 
 module.exports = router;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -47,3 +47,20 @@ exports.addOne = (req, res) => {
     return res.status(201).send({});
   }
 };
+
+exports.updateOne = (req, res) => {
+  let isExisting = false;
+  // Iterate through our recipes data, checking for desired object
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name === req.body.name) {
+      isExisting = true;
+      // Set desired object to request body
+      data[i] = req.body;
+      res.status(204).send();
+      break;
+    }
+  }
+  if (!isExisting) {
+    res.status(404).send({ error: "Recipe does not exist" });
+  }
+};
